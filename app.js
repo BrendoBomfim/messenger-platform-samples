53232ef9890afeba6a7918cec30dc3f6dae2d7ac
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2018-08-11 15:14:23 -0300

    Testing the download and convetion to base64

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const
-  request = require('request'),
+  request = require('request').defaults({ encoding: null }),
   express = require('express'),
   body_parser = require('body-parser'),
-  app = express().use(body_parser.json()); 
+  app = express().use(body_parser.json()),
+  fs = require('fs'),
+  http = require('http');
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -52,14 +54,15 @@ app.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, received_message) {
 
-
   if (received_message.text) {
     console.log(received_message.text);
-    
+
   } else if (received_message.attachments) {
     let attachment_url = received_message.attachments[0].payload.url;
     console.log(attachment_url);
-  } 
+    download(attachment_url);
+    
+  }
 }
 
 function handlePostback(sender_psid, received_postback) {
@@ -75,27 +78,36 @@ function handlePostback(sender_psid, received_postback) {
   callSendAPI(sender_psid, response);
 }
 
+function download(url) {
+  request.get(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+        console.log(data);
+    }
+  });
+};
+
 function afterPostback(sender_psid, received_postback) {
   let response;
   let payload = received_postback.payload;
 
   if (payload === 'yes') {
-    response = { 
-      "message":{
+    response = {
+      "message": {
         "attachment": {
           "type": "template",
           "payload": {
-             "template_type": "media",
-             "elements": [
-                {
-                   "media_type": "image",
-                   "url": "https://vignette.wikia.nocookie.net/meme/images/f/f4/Galo_Cego.jpg/revision/latest?cb=20170128222653&path-prefix=pt-br"
-                }
-             ]
+            "template_type": "media",
+            "elements": [
+              {
+                "media_type": "image",
+                "url": "https://vignette.wikia.nocookie.net/meme/images/f/f4/Galo_Cego.jpg/revision/latest?cb=20170128222653&path-prefix=pt-br"
+              }
+            ]
           }
-        }    
-      } 
-    } 
+        }
+      }
+    }
   } else if (payload === 'no') {
     response = { "text": "Apois mande do galo cego então..." }
   }
