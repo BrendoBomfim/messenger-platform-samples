fee73d23271bf9bfae93f1ca7a11f612f503f7cc
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2018-09-20 15:51:20 -0300

    loggin3

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ function download(url) {
   request.get(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
-        //console.log(data);
+        console.log(data);
         return data;
     }
   });
